ed32db2843d675a224553fb53d8e90fdb454ee6e
John Huang <little78926@gmail.com>
2016-04-14 15:48:47 -0700

    Add first route.

--- public/app.js
+++ public/app.js
@@ -1,8 +1,14 @@
 var app = angular.module('todo', []); // add [] for new module
-
 app.controller('homeController', home);
 
-function home() {
+app.$inject = ['$http'];
+
+function home($http) {
   var vm = this;
   vm.message = 'Angular JS is here';
+
+  var currentUser = $http({method: 'GET', url:'/current-user'});
+  currentUser.then(function (res) {
+    vm.currentUser = res.data;
+  })
 }
--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var port = process.env.PORT || 1337;
 
 app.use(express.static('./public/'));
 
+app.get('/current-user', function (req, res) {
+  var currentUser = {
+    name: 'John',
+    location: 'Irvine'
+  };
+  res.json(currentUser);
+})
+
 app.listen(port, function () {
   console.log('running on port: ' + port);
 })
