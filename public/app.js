303fdad0a1336fcfcbb07f60f28acd1c190ec4c4
John Huang <little78926@gmail.com>
2016-04-15 14:26:38 -0700

    I move the finised splice to backend

--- public/app.js
+++ public/app.js
@@ -17,22 +17,27 @@ app.$inject = ['$http', '$scope'];
 
 function todo($http, $scope) {
   var vm = this;
-  var todo = $http.get('/todo/John');
-  todo.then(function (res) {
-    vm.list = res.data.items;
-  })
+  getList();
+
+  function getList() {
+    var list = $http.get('/todo/John');
+    list.then(function (res) {
+      vm.list = res.data.items;
+    })
+  }
+
   vm.create = function () {
     console.log($scope.newItem);
     var newTodo = $http.post('/todo', {name: 'John', item: $scope.newItem});
     newTodo.then(function (res) {
-      vm.list = res.data.items;
+      getList();
     })
   }
   vm.finished = function (item) {
     console.log(item);
-    var checked = $http.get('/todo-check/'+item);
+    var checked = $http.put('/todo', {name: 'John', item: item});
     checked.then(function (res) {
-      vm.list = res.data.items;
+      getList();
     })
   }
 }
--- server.js
+++ server.js
@@ -59,11 +59,24 @@ app.post('/todo', function (req, res) {
   })
 })
 
-app.get('/todo-check/:item', function (req, res) {
+app.put('/todo', function (req, res) {
   console.log(req.url);
-  var position = todoArray.indexOf(req.params.item);
-  todoArray.splice(position, 1);
-  res.send(todoArray);
+  dbClient.connect(url, function (err, db) {
+    if (!err) {
+      var todos = db.collection('todos');
+      todos.find({name: req.body.name}).toArray(function (err, result) {
+        var itemArray = result[0].items;
+        var position = itemArray.indexOf(req.body.item);
+        itemArray.splice(position, 1);
+        todos.update({name: req.body.name}, {$set: {items: itemArray}}, function (err, result) {
+          db.close();
+        })
+        res.json(result[0]);
+      })
+    } else {
+      res.sendStatus(404);
+    }
+  })
 })
 
 app.listen(port, function () {
