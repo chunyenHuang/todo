edb8f20957be54ede91389b046f703353178993d
John Huang <little78926@gmail.com>
2016-04-15 14:02:07 -0700

    User can add their own item to todo list.

--- public/app.js
+++ public/app.js
@@ -6,25 +6,33 @@ function home($http) {
   var vm = this;
   vm.message = 'Angular JS is here';
 
-  var user = $http({method: 'GET', url:'/user'});
+  var user = $http({method: 'GET', url:'/user/John'});
   user.then(function (res) {
     vm.user = res.data;
   })
 }
 
-app.controller('todoeController', todo);
+app.controller('todoController', todo);
+app.$inject = ['$http', '$scope'];
 
-function todo($http) {
+function todo($http, $scope) {
   var vm = this;
   var todo = $http.get('/todo/John');
   todo.then(function (res) {
-    vm.list = res.data;
+    vm.list = res.data.items;
   })
+  vm.create = function () {
+    console.log($scope.newItem);
+    var newTodo = $http.post('/todo', {name: 'John', item: $scope.newItem});
+    newTodo.then(function (res) {
+      vm.list = res.data.items;
+    })
+  }
   vm.finished = function (item) {
     console.log(item);
     var checked = $http.get('/todo-check/'+item);
     checked.then(function (res) {
-      vm.list = res.data;
+      vm.list = res.data.items;
     })
   }
 }
