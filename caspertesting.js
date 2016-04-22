6e8502effcceff31d36c96c12604f26776dbf26f
John Huang <little78926@gmail.com>
2016-04-22 15:32:49 -0700

    Update casper and gulp

--- caspertesting.js
+++ caspertesting.js
@@ -1,20 +1,20 @@
-casper.test.begin('todo login', 6, function suite(test) {
-    casper.start("http://localhost:1337", function() {
-        test.assertTitle("Todo (Angular JS)", "Todo (Angular JS) homepage title is the one expected");
-        test.assertExists('form', "form is found");
-        this.fill('form', {
-            username: "John"
-        }, true);
-        test.assertExists('#show-list');
-        test.assertExists('#show-profile');
-    });
+casper.test.begin('todo login', function suite(test) {
+  casper.start("http://localhost:1337", function() {
+      test.assertTitle("Todo (Angular JS)", "Todo (Angular JS) homepage title is the one expected");
+      test.assertExists('form', "form is found");
+      this.fill('form', {
+          username: "John"
+      }, true);
+      test.assertExists('#show-list');
+      test.assertExists('#show-profile');
+  });
 
-    casper.then(function() {
-        test.assertTitle("Todo (Angular JS)", "Todo (Angular JS) title is ok");
-        test.assertUrlMatch('/', "user John has been submitted");
-    });
+  casper.then(function() {
+    this.click("a[href^='#/todo']");
+    test.assertUrlMatch('#/todo', 'link to todo page');
+  });
 
-    casper.run(function() {
-        test.done();
-    });
+  casper.run(function() {
+      test.done();
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var mocha = require('gulp-mocha');
 var casperJs = require('gulp-casperjs');
+var app = require('./app.js');
+var server = '';
 
 gulp.task('test', function() {
   return gulp.src('app.spec.js').pipe(mocha());
 })
 
-gulp.task('casper', function () {
-  gulp.src('caspertesting.js')
+gulp.task('casper', ['test-routes'], function () {
+  return gulp.src('caspertesting.js')
     .pipe(casperJs());
 });
 
@@ -19,4 +21,16 @@ gulp.task('test-routes', function () {
 
 gulp.task('go', function () {
   nodemon({script: 'app.js'}).on('start', ['test', 'casper', 'test-routes']);
+})
+
+gulp.task('travis', ['start', 'test-routes', 'casper', 'end'], function () {
+  server.close();
+});
+
+gulp.task('start', function() {
+  server = app.listen(1337);
+})
+
+gulp.task('end', ['casper'], function () {
+  server.close();
 })
